7135fbbb580911fbed3c758b7982eb3709dcd535
David Lam <Davidlamsgn@gmail.com>
2019-08-21 00:48:25 -0400

    MVP finished

--- src/oop/oop2.py
+++ src/oop/oop2.py
@@ -4,9 +4,10 @@
 # object is constructed.
 
 class GroundVehicle():
-    def __init__(self, num_wheels):
+    def __init__(self, num_wheels = 4):
         self.num_wheels = num_wheels
-
+    def drive(self):
+        return 'vroooom'
     # TODO
 
 
@@ -18,7 +19,12 @@ class GroundVehicle():
 # Override the drive() method in Motorcycle so that it returns "BRAAAP!!"
 
 # TODO
-
+class Motorcycle(GroundVehicle):
+    def __init__(self, num_wheels = 2):
+        super().__init__(num_wheels)
+        self.num_wheels = num_wheels
+    def drive(self):
+        return 'BRAAAP!!'
 vehicles = [
     GroundVehicle(),
     GroundVehicle(),
@@ -28,5 +34,6 @@ vehicles = [
 ]
 
 # Go through the vehicles list and print the result of calling drive() on each.
-
+my_list = [vehicle.drive() for vehicle in vehicles]
+print(my_list)
 # TODO
